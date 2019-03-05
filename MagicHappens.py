df2059a11cdd44eb6579dbacb22a81d3ba66da85
Kai Morita-McVey <kai.moritamcvey@gmail.com>
2019-03-04 18:27:09 -0800

    created readme, added test files, found starter code to work with

--- MagicHappens.py
+++ MagicHappens.py
@@ -1,1 +1,15 @@
+def compare():
+    with open("test_html/index1.html", "r") as file1:
+        with open("test_html/index2.html", "r") as file2:
+            same = set(file1).intersection(file2)
 
+        same.discard("\n")
+
+        for line in same:
+            print(line)
+        # with open("some_output_file.txt", "w") as file_out:
+        #     for line in same:
+        #         file_out.write(line)
+
+
+compare()
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Jjarke
+
+##This project should be run in a virtual enviroment with the following libraries installed:
+
+\*
+
+##The project is formatted using black
