981d703eff7c60cba89da89e6417689181310cc7
Ethan Wentworth <43764998+AwesomeSamIAm@users.noreply.github.com>
2019-03-06 15:03:26 -0800

    Update Output.py

--- Output.py
+++ Output.py
@@ -1,1 +1,18 @@
+#By Ethan Wentworth & Jacob H.
 
+from twilio.rest import Client
+
+# Your Account SID from twilio.com/console
+account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+# Your Auth Token from twilio.com/console
+auth_token  = "your_auth_token"
+
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+    to="+############", 
+    from_="+###########",
+    body="Testing testing one two three four five six ",minecraft, "!"  
+)
+
+print(message.sid)
