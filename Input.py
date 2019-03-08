79f15a2de507bc87373771885a4130949871809c
GiraffeWarrior <anenometentacles@gmail.com>
2019-03-08 14:14:19 -0800

    Update Input.py

--- Input.py
+++ Input.py
@@ -1,2 +1,5 @@
 
-input
+print("Welcome to our webscraping project! Please enter the URL \n of the website you would like to check, the length of \n time between cheques, and your twilio info!\n")
+x = ""
+print("Website URL: " + x)
+x = raw_input()
