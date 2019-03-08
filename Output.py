1b4e89eb32877a2cf2ec5fe1d362f423e0afdb23
Ethan Wentworth <43764998+AwesomeSamIAm@users.noreply.github.com>
2019-03-08 14:10:52 -0800

    Update Output.py

--- Output.py
+++ Output.py
@@ -9,8 +9,11 @@ message = client.messages.create(
     from_="+###########",
     # body="Testing testing one two three four five six ",minecraft, "!"  
     body="An update has occured between your two links, ", result, "." #1600 character limit
+  
 )
 
 print(message.sid)
 
 #What variables do we need for the output section? find_line_diff? d? result? list1 or 3?
+
+#https://www.twilio.com/docs/sms/send-messages
