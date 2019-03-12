610c0170f21b22b77020d19cc0a076716196d690
Jacob Humpherys <43765014+Jahuhu10@users.noreply.github.com>
2019-03-12 14:05:05 -0700

    Update Output.py

--- Output.py
+++ Output.py
@@ -8,7 +8,7 @@ message = client.messages.create(
     to="+############", 
     from_="+###########",
     # body="Testing testing one two three four five six ",minecraft, "!"  
-    body="An update has occured between your two links, ", result, "." #1600 character limit
+    body="This many characters have been changed, ", result, "These were the characters that were added, or removed", result, "." #1600 character limit
   
 )
 
