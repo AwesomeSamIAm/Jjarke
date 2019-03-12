14ec03ab5537e9ba22c2e42a713d709d83549523
Kai Morita-McVey <kai.moritamcvey@gmail.com>
2019-03-12 13:58:26 -0700

    added stuff

--- obtain_html.py
+++ obtain_html.py
@@ -1,6 +1,26 @@
-# import beautifulsoup4 as bs4
+from bs4 import BeautifulSoup
+import requests
 
 
-def get_site_html(url_1, *elements):
-    print(url_1, elements)
-    return ()
+def get_site_html(url):
+    print(url)
+    req = requests.get(url)
+    soup = BeautifulSoup(req.content, "html.parser")
+    print(soup.prettify())
+    return soup
+
+
+def html_parser(html_2_parse, elements):
+    for x in elements:
+        print(x)
+    return "in progress"
+
+
+def main():
+    site_html = get_site_html("https://kaimoritamcvey.me")
+    elements = ["p", "h1", "h2", "h3"]
+    print(html_parser(site_html, elements))
+
+
+if __name__ == "__main__":
+    main()
