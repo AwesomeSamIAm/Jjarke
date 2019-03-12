106f2b39aa17ca23939f073cbfacf9ac16e7507f
GiraffeWarrior <anenometentacles@gmail.com>
2019-03-12 14:35:41 -0700

    Update Input.py

--- Input.py
+++ Input.py
@@ -1,5 +1,9 @@
 
+
 print("Welcome to our webscraping project! Please enter the URL \n of the website you would like to check, the length of \n time between cheques, and your twilio info!\n")
-x = ""
-print("Website URL: " + x)
-x = raw_input()
+weburl = raw_input("Website URL: ")
+interv = raw_input("Time Interval: ")
+account_sid= raw_input("Account Sid: ")
+auth_token= raw_input("Authorization Token: ")
+twinum = raw_input("Twilio Number: ")
+num = raw_input("Destination Number: ")
