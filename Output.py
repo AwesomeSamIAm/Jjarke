2c9ab107bbfba963771595d1f03bef8fd5cf051c
Ethan Wentworth <43764998+AwesomeSamIAm@users.noreply.github.com>
2019-03-14 15:11:10 -0700

    Update Output.py

--- Output.py
+++ Output.py
@@ -1,19 +1,20 @@
 #By Ethan Wentworth & Jacob H.
 
 from twilio.rest import Client
+Fpart = "This many characters have been changed, "
+Mpart = "Key: + is added, - is removed. These were the characters that were added, or removed"
 
-#minecraft = 7
+#list1 is each character added
+#list2 is number total
 
 message = client.messages.create(
     to="+############", 
-    from_="+###########",
-    # body="Testing testing one two three four five six ",minecraft, "!"  
-    body="This many characters have been changed, ", result, " Key: + is added, - is removed. These were the characters that were added, or removed", result, "." #1600 character limit
-  
-)
+    from_="+###########",  
+    body=""
+   )
 
-print(message.sid)
+print(Fpart+list2+Mpart+list1)
 
 #What variables do we need for the output section? find_line_diff? d? result? list1 or 3?
 
-#https://www.twilio.com/docs/sms/send-messages
+#https://www.twilio.com/docs/sms/send-messages 
