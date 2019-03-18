f128c0d092d55650996e5f45b21a63e92ed7fad6
Ethan Wentworth <43764998+AwesomeSamIAm@users.noreply.github.com>
2019-03-18 13:49:31 -0700

    Update Output.py

--- Output.py
+++ Output.py
@@ -15,6 +15,5 @@ message = client.messages.create(
 
 print(Fpart+list2+Mpart+list1)
 
-#What variables do we need for the output section? find_line_diff? d? result? list1 or 3?
 
 #https://www.twilio.com/docs/sms/send-messages 
