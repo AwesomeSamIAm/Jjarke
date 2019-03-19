e9c6f2749f3bcd323869bb735958ca9fec9c7ee6
Kai Morita-McVey <kai.moritamcvey@gmail.com>
2019-03-19 12:33:12 -0700

    Major changes to comparison algorithm, supports

--- MagicHappens.py
+++ MagicHappens.py
@@ -1,85 +1,54 @@
-# Option 1, does not account for multiple of the same differences in the files, no record of place of difference
-# def compare():
-#     with open("test_html/index1.html", "r") as file1:
-#         with open("test_html/index2.html", "r") as file2:
-#             same = set(file1).difference(file2)
-
-#         same.discard("\n")
-
-#         for line in same:
-#             print(line)
-#         # with open("some_output_file.txt", "w") as file_out:
-#         #     for line in same:
-#         #         file_out.write(line)
-
-
-# compare()
-#################################################################################################################
-
-# Option 2, uses a library to compare total changes, then if there are changes looks charicter by charicter
-# import filecmp
-
-# # returns true if the files are the
-# if filecmp.cmp("test_html/index1.html", "test_html/index2.html"):
-
-#     print("The files are the same")
-# else:
-#     print("The files are diffrent, finding different charicters")
-#     # Goes to the compare_character def
-#     compare_character()
-
-
-# def compare_character():
-#     pass
-
-#################################################################################################################
-
-# Option 3 is to use the difflib library to find differences between two lines of text read from the html page
 from difflib import Differ
-
-# from .obtain_html import get_site_html
+from bs4 import BeautifulSoup
+import requests
+import time
 
 
 # Set both text strings
 def get_html(url, *elements):
-    return get_site_html(url, elements)
+    def get_site_html():
+        # print(url)
+        req = requests.get(url)
+        soup = BeautifulSoup(req.content, "html.parser")
+        # print(soup.prettify())
+        return soup
+
+    def html_parser():
+        element_list = []
+        for x in elements:
+            # print(x)
+            element_list += html_soup.find_all(x)
+        return element_list
+
+    html_soup = get_site_html()
+    parsed_html = html_parser()
+    return parsed_html
 
 
-# Function that takes two strings, compares them, then returns the number of changes and specific changes
-def find_line_diff(str1, str2):
-    # Initialize the Differ class as d
-    d = Differ()
-
-    # Create a list of the differences found by the compare method
-    result = list(d.compare(str1, str2))
-
-    # Print the number of changes (entries in the list), then the changes. The key is as follows:
-    # '- ' 	line unique to sequence 1
-    # '+ ' 	line unique to sequence 2
-    # '  ' 	line common to both sequences
-    # '? ' 	line not present in either input sequence
-    return len(result), result
-
-
-def send_changes(text1, text2):
-    # Print the changes found between text1 and text2 returned by find_line_diff
-    print(find_line_diff(text1, text2))
-
-    # Creates two lists from the tuple returned by find_line_diff, list2 is the amound of differences,
-    # while list1 is the list of actual differences
-    list2, list1 = find_line_diff(text1, text2)
-    print(list2)
-
-    for x in list1:
-        if "+" in x:
-            print(x)
-        elif "-" in x:
-            print("-" + x)
-        else:
-            pass
+def list_diff(list1, list2):
+    diff_list = list(set(list1).symmetric_difference(set(list2)))
+    if not diff_list:
+        x = len(list1) - len(list2)
+        print(
+            f"There are {x} changes, however strings have been moved around. Here is what has moved:"
+        )
+        print(*[x for x, y in zip(list1, list2) if x != y], sep="\n")
+    else:
+        print(
+            "The lines in list1 that differ from files at the same space in list2, these can be assumed to be lines moved around"
+        )
+        print(*[x for x, y in zip(list1, list2) if x != y], sep="\n")
+        print(
+            "These list elements are diffrent from listx compared to listy, including items moved around, added, or changed. These could be considerd to have been removed"
+        )
+        print(*[item for item in list1 if item not in list2], sep="\n")
+        print(
+            "These list elements are diffrent from listy compared to listx, including items moved around, added, or changed. These could be considerd to have been added"
+        )
+        print(*[item for item in list2 if item not in list1], sep="\n")
 
 
-# print(get_html("kaimoritamcvey.me", "p", "h1", "h2", "h3"))
-print(find_line_diff("abc", "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore, ea,exer"))
-print("##########################################################")
-print(send_changes("abc", "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore, ea,exer"))
+a = get_html("http://127.0.0.1:5500/Jjarke/test_html/index1.html", "p")
+time.sleep(15)
+b = get_html("http://127.0.0.1:5500/Jjarke/test_html/index1.html", "p")
+list_diff(a, b)
