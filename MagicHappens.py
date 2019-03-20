21fdcbad0ae6218f799414dbcb253cc580422174
Kai Morita-McVey <kai.moritamcvey@gmail.com>
2019-03-20 09:32:31 -0700

    Cleaned up code, fixed comments

--- MagicHappens.py
+++ MagicHappens.py
@@ -29,41 +29,27 @@ def list_diff(list1, list2):
     place_differ = []
     removed_strings = []
     added_strings = []
+
+    # Check to see if the only change is moving the order
     if not diff_list:
         x = len(list1) - len(list2)
-        # print(
-        #     f"There are {x} changes, however strings have been moved around. Here is what has moved:"
-        # )
         place_differ = [x for x, y in zip(list1, list2) if x != y]
-        # print(*moved_strings, sep="\n")
         return place_differ, removed_strings, added_strings
+
     else:
         # obtain the strings that changed, and list them
         place_differ = [x for x, y in zip(list1, list2) if x != y]
         added_strings = [item for item in list2 if item not in list1]
         removed_strings = [item for item in list1 if item not in list2]
-
-        ## Print the resulting lists of changes
-        # print(
-        #     "The lines in list1 that differ from files at the same space in list2, these can be assumed to be lines moved around"
-        # )
-        # print(*place_differ, sep="\n")
-        # print(
-        #     "These list elements are diffrent from list1 compared to list2, including items moved around, added, or changed. These could be considerd to have been removed"
-        # )
-        # print(*removed_strings, sep="\n")
-        # print(
-        #     "These list elements are diffrent from list2 compared to list1, including items moved around, added, or changed. These could be considerd to have been added"
-        # )
-        # print(*added_strings, sep="\n")
-
-        # Return the lists of changes
         return place_differ, removed_strings, added_strings
 
 
+# Find number of characters that changed
+# TODO add suport to ignore line place changes
 def char_length_checker(
     place_differ, removed_strings, added_strings, char_check_length=3
 ):
+    # Store total character changes in a variable
     changed_characters = 0
     for x in range(len(place_differ)):
         changed_characters += len(place_differ[x])
@@ -72,16 +58,18 @@ def char_length_checker(
     for x in range(len(added_strings)):
         changed_characters += len(added_strings[x])
     print(changed_characters)
+
+    # If total character changes are equal or equal to amount specified return TRUE
     if changed_characters >= char_check_length:
         return 1
     else:
         return 0
 
 
+# This is for testing, will not make master
 list1 = ["abc", "123", "ddd", "bbb"]
 list2 = ["123", "abc", "aaa", "eee", "hans get ze luger"]
-list2 = ["abc", "123", "bbb", "ddd"]
-
+# list2 = ["abc", "123", "bbb", "ddd"]
 
 place_differ, removed_strings, added_strings = list_diff(list1, list2)
 if char_length_checker(place_differ, removed_strings, added_strings) == True:
