7665f0f656e5dd1cbc67db3c3316aeb55ad6e58d
Jacob Humpherys <43765014+Jahuhu10@users.noreply.github.com>
2019-03-20 13:52:46 -0700

    Update Output.py

--- Output.py
+++ Output.py
@@ -1,19 +1,25 @@
 #By Ethan Wentworth & Jacob H.
 
+#By Ethan Wentworth & Jacob H.
+
+import twilio
 from twilio.rest import Client
 Fpart = "This many characters have been changed, "
 Mpart = ". Key: + is added, - is removed. These were the characters that were added, or removed "
 
 #list1 is each character added
+
 #list2 is number total
 
-message = client.messages.create(
-    to="+############", 
-    from_="+###########",  
-    body=""
-   )
 
-print(Fpart+list2+Mpart+list1)
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+                .create(
+                     body= (Fpart+list2+Mpart+list1),
+                     from_= twinum,
+                     to= num
+                 )
 
 
 #https://www.twilio.com/docs/sms/send-messages 
