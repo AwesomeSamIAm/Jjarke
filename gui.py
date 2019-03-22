5e139b01e297c2604e00628ec557016bcaab7d47
GiraffeWarrior <anenometentacles@gmail.com>
2019-03-22 14:15:31 -0700

    Update gui.py

--- gui.py
+++ gui.py
@@ -20,3 +20,41 @@ app.addButton("Submit", save)
 app.go()
 
 
+# import appJar
+# from  appJar import gui
+# app = gui()
+# web_url = 0
+# twi= 0
+# variables = ["web_url", "twi"]
+# # add & configure widgets - widgets get a name, to help referencing them later
+# allentries = ["ent1", "ent2"]
+# app.addLabel("title", "Welcome to appJar")
+# app.setLabelBg("title", "red")
+# app.addLabel("url", "Website URL: ", 0, 0)
+# app.addEntry("weburl", 0, 1)
+# app.addLabel("twi", "Twilio:  ", 1, 0)
+# app.addEntry("2", 1, 1)
+
+
+# def save() :
+#     e = app.getAllEntries()
+#     def death():
+#         for i in e:
+#             a = e[i]
+#             print(i)
+
+#     # e.keys = e.values
+#     print(e)
+#     # for i in range(len(e)):
+        
+  
+       
+        
+
+        
+
+
+   
+
+# app.addButton("Submit", save)
+# app.go()
