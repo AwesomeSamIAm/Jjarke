27579f042b2b5cc5afb6a61eeebd68f799df9df6
GiraffeWarrior <anenometentacles@gmail.com>
2019-04-02 09:22:42 -0700

    Update Input.py
    Changed to python 3 syntax

--- Input.py
+++ Input.py
@@ -1,9 +1,7 @@
-
-
-print("Welcome to our webscraping project! Please enter the URL \n of the website you would like to check, the length of \n time between cheques, and your twilio info!\n")
-weburl = raw_input("Website URL: ")
-interv = raw_input("Time Interval: ")
-account_sid= raw_input("Account Sid: ")
-auth_token= raw_input("Authorization Token: ")
-twinum = raw_input("Twilio Number: ")
-num = raw_input("Destination Number: ")
+print("Welcome to our webscraping project! Please enter the URL \n of the website you would like to check, the length of \n time between checks, and your twilio info!\n")
+weburl =input("Website URL: ")
+interv =input("Time Interval: ")
+account_sid=input("Account Sid: ")
+auth_token= input("Authorization Token: ")
+twinum =input("Twilio Number: ")
+num = input("Destination Number: ")
