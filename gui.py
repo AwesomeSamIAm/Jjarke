62d6d533ce0a20c554549adb81313e4cc88ffee7
GiraffeWarrior <anenometentacles@gmail.com>
2019-04-02 09:20:09 -0700

    Update gui.py
    Wrote a hard code version of the window that takes the contents of data entries and assigns them to each variable name. The variables are global, and so should be easily called to another file when this one is imported. There are no checks to see if submitted answers are usable, though.

--- gui.py
+++ gui.py
@@ -3,43 +3,56 @@ import appJar
 from  appJar import gui
 app = gui()
 
-# window declarations
-app.addLabel("title", "Welcome to appJar")
+#global variable initializations
+weburl = 0
+interv = 0
+sid = 0
+authtok = 0
+twinum = 0
+destnum = 0
+
+#title declaration
+app.addLabel("title", "Welcome to our webscraper! Please enter the information below.", 0,0,2)
 app.setLabelBg("title", "red")
 
-#label and entry declarations
-app.addLabel("url", "Website URL: ", 0, 0)
-app.addEntry("weburl", 0, 1)
-app.addLabel("int", "Time Interval:  ", 1, 0)
-app.addEntry("interv", 1, 1)
-app.addLabel("asid", "Account sid: ", 2, 0 )
-app.addEntry("sid",2,1 )
-app.addLabel("auth", "Authorization Token: ", 3, 0)
-app.addEntry("authtok", 3,1)
-app.addLabel("twi", "Twilio Number: ", 4,0)
-app.addEntry("twinum", 4,1)
-app.addLabel("dest", "Destination Number: ", 5,0)
-app.addEntry("destnum", 5,1)
-#button function- saves entries to respective variable names
+#labels and entry declarations
+app.addLabel("url", "Website URL: ", 1, 0)
+app.addEntry("weburl", 1, 1)
+app.addLabel("int", "Time Interval:  ", 2, 0)
+app.addEntry("interv", 2, 1)
+app.addLabel("asid", "Account sid: ", 3, 0 )
+app.addEntry("sid",3,1 )
+app.addLabel("auth", "Authorization Token: ", 4, 0)
+app.addEntry("authtok", 4,1)
+app.addLabel("twi", "Twilio Number: ", 5,0)
+app.addEntry("twinum", 5,1)
+app.addLabel("dest", "Destination Number: ", 6,0)
+app.addEntry("destnum", 6,1)
+#submit buttton code(global variable calls, global variable assignments)
 def save() :
-   weburl = app.getEntry("weburl")
-   interv = app.getEntry("interv")
-   sid = app.getEntry("sid")
-   authtok = app.getEntry("authtok")
-   twinum = app.getEntry("twinum")
-   destnum = app.getEntry("destnum")
+    global weburl
+    global interv
+    global sid
+    global authtok
+    global twinum
+    global destnum
+    weburl = app.getEntry("weburl")
+    interv = app.getEntry("interv")
+    sid = app.getEntry("sid")
+    authtok = app.getEntry("authtok")
+    twinum = app.getEntry("twinum")
+    destnum = app.getEntry("destnum")
+   
 
-   #printed variable names to check
-   print(weburl)
-   print(interv)
-   print(sid)
-   print(authtok)
-   print(twinum)
-   print(destnum)
+   
 
-#button declaration and window initialization
+
+
+   
+#button declaration and app initialization
 app.addButton("Submit", save)
 app.go()
+
 
 
 
