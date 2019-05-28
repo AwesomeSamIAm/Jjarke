0175f20098fcbe88a43edc7a4b10e5171dfa5341
Ethan Wentworth <43764998+AwesomeSamIAm@users.noreply.github.com>
2019-05-28 14:39:28 -0700

    Update Output.py

--- Output.py
+++ Output.py
@@ -2,21 +2,21 @@
 
 import twilio
 from twilio.rest import Client
-Fpart = "This many characters have been changed, "
-Mpart = ". Key: + is added, - is removed. These were the characters that were added, or removed "
+def main(list1, list2, input_dict):
+  Fpart = "This many characters have been changed, "
+  Mpart = ". Key: + is added, - is removed. These were the characters that were added, or removed "
 
-#list1 is each character added
+   #list1 is each character added
 
-#list2 is number total
+  #list2 is number total
+  
+  client = Client(input_dict["account_sid"], input_dict["auth_token"])
 
-
-client = Client(account_sid, auth_token)
-
-message = client.messages \
+  message = client.messages \
                 .create(
                      body= (Fpart+list2+Mpart+list1),
-                     from_= twinum,
-                     to= num
+                     from_= input_dict["twinum"],
+                     to= input_dict["num"]
                  )
 
 
