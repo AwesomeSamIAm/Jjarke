f4a7bbfb20900af2f6b2c8ecf1ec3cc8da477d87
KeKoaM <kai.moritamcvey@gmail.com>
2019-05-30 13:31:47 -0700

    added TODO, fixed list usage and varaible names

--- Output.py
+++ Output.py
@@ -1,23 +1,30 @@
-#By Ethan Wentworth & Jacob H.
-
+# By Ethan Wentworth & Jacob H.
+# TODO add message length check
 import twilio
 from twilio.rest import Client
+
+
+def list_to_string(list):
+    # str = ",".join(list)
+    str1 = "".join(str(e) for e in list)
+    return str1
+
+
 def main(list1, list2, input_dict):
-  Fpart = "This many characters have been changed, "
-  Mpart = ". Key: + is added, - is removed. These were the characters that were added, or removed "
+    print(list1)
+    str1 = list_to_string(list1)
+    str2 = list_to_string(list2)
+    Fpart = "This many characters have been changed, "
+    Mpart = ". Key: + is added, - is removed. These were the characters that were added, or removed "
+    # list1 is each character added
 
-   #list1 is each character added
+    # list2 is number total
 
-  #list2 is number total
-  
-  client = Client(input_dict["account_sid"], input_dict["auth_token"])
+    client = Client(input_dict["sid"], input_dict["authtok"])
 
-  message = client.messages \
-                .create(
-                     body= (Fpart+list2+Mpart+list1),
-                     from_= input_dict["twinum"],
-                     to= input_dict["num"]
-                 )
-
-
-#https://www.twilio.com/docs/sms/send-messages 
+    message = client.messages.create(
+        body=(Fpart + str2 + Mpart + str1),
+        from_=input_dict["twinum"],
+        to=input_dict["destnum"],
+    )
+    print(message.sid)
