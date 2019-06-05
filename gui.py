e575282e55e0dca62cc452edfdaf784f879c3198
Jason Chong <joeseongchang@gmail.com>
2019-06-05 14:21:09 -0700

    updated the gui file

--- gui.py
+++ gui.py
@@ -35,6 +35,9 @@ def main():
     app.addEntry("destnum", 6, 1)
     app.addLabel("char", "Minimum Character Change: ", 7, 0)
     app.addEntry("character_length", 7, 1)
+    app.addLabel("tag", "Tag(s): ", 8, 0)
+    app.addEntry("tags", 8, 1)
+     
     # submit buttton code(global variable calls, global variable assignments)
     def save():
         global variables
